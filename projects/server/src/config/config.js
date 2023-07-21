940d764c09c9919197ce75eb131667f413fa3d69
ameliafatmamutia <ameliafatmam@gmail.com>
2023-07-21 23:09:16 +0700

    Fixing button in branch store management

--- projects/server/src/config/config.js
+++ projects/server/src/config/config.js
@@ -2,25 +2,46 @@ const { join } = require("path");
 require('dotenv').config({ path: join(__dirname, '../.env') });
 
 module.exports = {
+  // development: {
+  //   username: "jcwdol0905",
+  //   password: "groupol0905",
+  //   database: "jcwdol0905",
+  //   host: "127.0.0.1",
+  //   dialect: "mysql"
+  // },
+  // test: {
+  //   username: "jcwdol0905",
+  //   password: "groupol0905",
+  //   database: "jcwdol0905",
+  //   host: "127.0.0.1",
+  //   dialect: "mysql"
+  // },
+  // production: {
+  //   username: "jcwdol0905",
+  //   password: "groupol0905",
+  //   database: "jcwdol0905",
+  //   host: "127.0.0.1",
+  //   dialect: "mysql"
+  // },
   development: {
-    username: "jcwdol0905",
-    password: "groupol0905",
-    database: "jcwdol0905",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     host: "127.0.0.1",
-    dialect: "mysql"
+    dialect: "mysql",
   },
   test: {
-    username: "jcwdol0905",
-    password: "groupol0905",
-    database: "jcwdol0905",
+    username: "root",
+    password: null,
+    database: "database_test",
     host: "127.0.0.1",
-    dialect: "mysql"
+    dialect: "mysql",
   },
   production: {
-    username: "jcwdol0905",
-    password: "groupol0905",
-    database: "jcwdol0905",
+    username: "root",
+    password: null,
+    database: "database_production",
     host: "127.0.0.1",
-    dialect: "mysql"
-  }
+    dialect: "mysql",
+  },
 };
